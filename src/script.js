f5807ab9688881f8eea5edda540327a51fea8dfd
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-28 20:09:56 -0300

    Added hide method for uncaptured page

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    http://tampermonkey.net/ @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        *://*/*
@@ -22,6 +21,41 @@
 
         return favicon.href;
     }
+
+	function createLoader() {
+		const html = document.createElement("html");
+		const style =
+			"<style>" +
+			".mid {" +
+			"    text-align: center !important;" +
+			"    margin: 0 auto !important;" +
+			"}" +
+			".loader {" +
+			"    border: 16px solid #f3f3f3;" +
+			"    border-top: 16px solid #3498db;" +
+			"    border-radius: 50%;" +
+			"    width: 120px;" +
+			"    height: 120px;" +
+            "    margin: inherit;" +
+			"    animation: spin 2s ease-in-out infinite;" +
+			"}" +
+			"@keyframes spin {" +
+			"    0% { transform: rotate(0deg); }" +
+			"    100% { transform: rotate(360deg); }" +
+			"}" +
+			"</style>";
+
+		const markdown =
+			"<div class='mid'>" +
+			"    <h1>Loading, please wait...</h1>" +
+			"    <div class='loader'></div>" +
+			"</div>";
+
+		html.innerHTML += "<body>" + style + markdown + "</body>";
+		return html;
+	}
+
+	window.loadingFacade = false;
 
     window.onmousedown = async (e) => {
 		if (e.ctrlKey && e.shiftKey) {
@@ -54,8 +88,8 @@
             sessionStorage.setItem("page-title", document.title);
             sessionStorage.setItem("page-favicon", getFavicon());
 
-            await GM.getValue("page-capture", null).then((val) => {
-                if (val != null) {
+            await GM.getValue("page-capture", null).then(async (val) => {
+                if (val != null && !window.loadingFacade) {
                     //window.res = Array.from(document.body.childNodes);
                     //window.res.forEach(e => document.body.removeChild(e));
                     document.body.style.display = "none";
@@ -63,27 +97,40 @@
                     window.dummyHtml = document.createElement("html");
                     window.dummyHtml.innerHTML = JSON.parse(val);
                     document.documentElement.appendChild(window.dummyHtml);
-                }
-                sessionStorage.setItem("page-capture", val);
-            });
-            await GM.getValue("page-passwd", null).then((val) => {
-                sessionStorage.setItem("page-passwd", val);
+
+					await GM.getValue("page-passwd", null).then((val) => {
+						sessionStorage.setItem("page-passwd", val);
+					});
+                } else {
+					document.body.style.display = "none";
+					window.pageLoader = !window.pageLoader
+						? createLoader()
+						: window.pageLoader;
+					document.documentElement.appendChild(window.pageLoader);
+
+					window.loadingFacade = true;
+				}
             });
         } else if (e.shiftKey && e.altKey) {
-            //if (window.res != undefined) {
-            const passwd = prompt("Insert password.");
-            if (sessionStorage.getItem("page-passwd") == passwd) {
-                document.documentElement.removeChild(window.dummyHtml);
-                //window.res.forEach(e => document.body.appendChild(e));
-                document.body.style.display = "";
-                document.title = sessionStorage.getItem("page-title");
+			if (!window.loadingFacade) {
+				const passwd = prompt("Insert password.");
 
-                const favicon = document.createElement("link");
-                favicon.rel = "icon";
-                favicon.href = sessionStorage.getItem("page-favicon");
-                document.body.appendChild(favicon);
-            }
-            //}
+				if (sessionStorage.getItem("page-passwd") == passwd) {
+					document.documentElement.removeChild(window.dummyHtml);
+					//window.res.forEach(e => document.body.appendChild(e));
+					document.body.style.display = "";
+					document.title = sessionStorage.getItem("page-title");
+
+					const favicon = document.createElement("link");
+					favicon.rel = "icon";
+					favicon.href = sessionStorage.getItem("page-favicon");
+					document.body.appendChild(favicon);
+				}
+			} else {
+				document.body.style.display = "";
+				document.documentElement.removeChild(window.pageLoader);
+                window.loadingFacade = false;
+			}
         }
     };
 })();
